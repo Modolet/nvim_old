524863a1f267bf125d3ae173aebbaf33032e5b8c
modolet <modolet@163.com>
2024-01-17 17:30:57 +0800

    调整加载顺序，加入主题

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
 if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
 
 for _, source in ipairs {
+	"base.options",
 	"base.lazy",
-	"base.autocmds"
+	"base.autocmds",
+	"base.mappings",
 } do
 	local status_ok, fault = pcall(require, source)
 	if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
 end
+
+local colorscheme = "nord"
+pcall(vim.cmd.colorscheme, colorscheme)
